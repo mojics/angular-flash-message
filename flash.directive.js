9ded1edaab197bcef523f756595a9274aac92172
Ronald Allan MOjica <ronald@eurasiapayment.com>
2015-06-19 23:04:30 +0800

    Initial commit

---
+++ flash.directive.js
@@ -0,0 +1,41 @@
+angular.module('angularFlash',[]);
+
+(function(app) {
+'use strict';
+  
+  app.directive('angularFlash', [AngularFlash]);
+
+  function AngularFlash() {
+    return {
+      scope: {
+        text: '@'        
+      },
+      restrict: 'E',
+      replace: true,
+      template: '<div class="angular-flash {{type()}}" ng-show="showFlag() === true">{{message()}}</div>',
+      controller: ['$scope','$attrs','$flash', FlashCtrl]
+    };
+  }
+
+
+  function FlashCtrl($scope, $attrs, $flash){
+    $scope.message = function(){
+      return $flash.message;
+    };
+
+    $scope.showFlag = function(){
+      return $flash.isShown();
+    };
+
+    $scope.type = function(){
+      return $flash.type;
+    };
+
+    $scope.$on('$destroy',function(){
+      $flash.hide();
+    });
+  }
+
+  
+
+})(angular.module('angularFlash'));
---
+++ flash.factory.js
@@ -0,0 +1,39 @@
+(function(app) {
+'use strict';
+    
+  app.factory('$flash', flashFactory);   
+
+  function flashFactory(){
+    return {
+      message: null,
+      showFlag: false,
+      type: 'error', // default is error type
+      types: ['error','warning','success'],
+      
+      isShown: function(){
+        return this.showFlag;
+      },
+      show: function(opts){
+        if(opts && opts.hasOwnProperty('message')){
+          this.message = opts.message;
+          this.showFlag = true;  
+          
+          if(this.types.indexOf(opts.type) > -1){
+            this.type = opts.type;
+          }
+
+        }else{
+          this.message = '';
+          this.showFlag = false;  
+        }
+
+        return this;
+      },
+      hide: function(){
+        this.message = null;
+        this.showFlag = false;
+      }
+    };
+  }
+
+})(angular.module('angularFlash'));
